43c95414a0047afbb707dc7e1c7c94a4de2eead4
Adarsh Dubey <adarshdubeydelhi@gmail.com>
2026-02-10 16:47:35 +0530

    New Chages are done

--- src/components/Filtered/FilterGrid.tsx
+++ src/components/Filtered/FilterGrid.tsx
@@ -23,7 +23,7 @@ import Link from "next/link";
 //   },
 // ];
 
-export default function FiterGrid({packages , setIsOpen} : {packages : any[], setIsOpen : any}) {
+export default function FilterGrid({packages , setIsOpen} : {packages : any[], setIsOpen : any}) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
 
